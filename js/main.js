f844a354f620f7d9f9d43ef7e59a7b975602c280
imarjuk <1marjuk@gmail.com>
2025-03-02 23:53:21 +0600

    final commit done'

--- js/main.js
+++ js/main.js
@@ -5,12 +5,17 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+    {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-1').disabled= true;
-        document.getElementById('btn-1').classList.add('bg-gray-300');       
+        document.getElementById('btn-1').classList.add('bg-gray-300'); 
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p);      
     }
         
  })
@@ -21,12 +26,17 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+    {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-2').disabled= true;
-        document.getElementById('btn-2').classList.add('bg-gray-300');       
+        document.getElementById('btn-2').classList.add('bg-gray-300'); 
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p);        
     }
         
  })
@@ -38,12 +48,17 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+   {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-3').disabled= true;
-        document.getElementById('btn-3').classList.add('bg-gray-300');       
+        document.getElementById('btn-3').classList.add('bg-gray-300');  
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p);      
     }
         
  })
@@ -55,12 +70,17 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+    {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-4').disabled= true;
-        document.getElementById('btn-4').classList.add('bg-gray-300');       
+        document.getElementById('btn-4').classList.add('bg-gray-300');
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p); ;        
     }
         
  })
@@ -72,12 +92,17 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+    {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-5').disabled= true;
-        document.getElementById('btn-5').classList.add('bg-gray-300');       
+        document.getElementById('btn-5').classList.add('bg-gray-300'); 
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p);        
     }
         
  })
@@ -89,12 +114,18 @@
     const completedTask = document.getElementById('total-task');
     const contCompletedTask = parseInt(completedTask.innerText) + 1 ;
 
-    if (conTask >= 5 && contCompletedTask >= 23 ){
+
+    {
         alert('board Update Successfully');
         task.innerText = '0'+conTask;
         completedTask.innerText = contCompletedTask;
         document.getElementById('btn-6').disabled= true;
-        document.getElementById('btn-6').classList.add('bg-gray-300');       
+        document.getElementById('btn-6').classList.add('bg-gray-300');   
+        const activity = document.getElementById("activity")
+        const p = document.createElement("p");
+        p.innerText = "You have Complete The Task Add Dark Mode at 12:48:15 ";
+        p.classList.add('text-sm', 'font-light', 'p-2', 'bg-[#F4F7FF]');
+        activity.appendChild(p);      
     }
         
  })
